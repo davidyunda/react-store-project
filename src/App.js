04cf4398c153b1d7b05f24506b58df2ddda148f4
davidyunda <nm5128kk@go.minnstate.edu>
2023-03-19 20:18:19 -0500

    change border color and height

--- src/App.js
+++ src/App.js
@@ -21,10 +21,13 @@ function App() {
       <section className="cards-list">
         {cards}
       </section>
-      <div className="card-question border-2 rounded-lg border-slate-500 text-lg font-medium box-content w-64 h-36 text-center content-between">
-        <h3>
-          Please, Contact us if you have any questions at: contact-us@qatustore.com or send us a text message at (612)-306-3831
-        </h3>
+      <div className="card-question border-2 border-blue-800 rounded-lg border-slate-500 text-lg font-medium box-content w-80 h-36 text-center content-between">
+        <p>
+          Please, Contact us if you have any questions at:
+        </p>
+        <p>
+          contact-us@qatustore.com or send us a text message at (612)-306-3831
+        </p>
       </div>
     </div>
   );
